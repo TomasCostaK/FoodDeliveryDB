7b3543432a650361d43836a4ca864f0e67158b7c
jmnmv12 <j.vasconcelos99@gmail.com>
2019-05-21 20:27:25 +0100

    login form

--- App/FoodDelivery/FoodDelivery/Form1.Designer.cs
+++ App/FoodDelivery/FoodDelivery/Form1.Designer.cs
@@ -34,6 +34,40 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -44,7 +78,6 @@
             this.label1.Size = new System.Drawing.Size(76, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "LoginPage\r\n";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
@@ -83,7 +116,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(700, 419);
+            this.button1.Location = new System.Drawing.Point(700, 400);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(81, 25);
             this.button1.TabIndex = 5;
@@ -91,19 +124,326 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(627, 483);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Are you a Restaurant? Click here\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Location = new System.Drawing.Point(342, 144);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1462, 714);
+            this.panel1.TabIndex = 7;
+            this.panel1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(662, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "RestaurantLogin\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(603, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Login";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(674, 299);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 34);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "RestaurantID:\r\n\r\n";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(639, 333);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(603, 467);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 25);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Go Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(603, 419);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(248, 29);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "SignUp Restaurant\r\n";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(700, 442);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(81, 25);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Sign Up";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.textBox13);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.textBox12);
+            this.panel2.Controls.Add(this.textBox11);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.textBox10);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.textBox9);
+            this.panel2.Controls.Add(this.textBox8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.textBox6);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(424, 130);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1459, 711);
+            this.panel2.TabIndex = 14;
+            this.panel2.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(721, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "RestaurantLogin\r\n";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(687, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(186, 22);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(585, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Username*:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(585, 165);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Password*:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(687, 163);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(186, 22);
+            this.textBox5.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(585, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Name*:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(687, 216);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(186, 22);
+            this.textBox6.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(585, 266);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 17);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Contact*:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(687, 263);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(186, 22);
+            this.textBox7.TabIndex = 23;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(588, 309);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 37);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "Image";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(687, 316);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(186, 22);
+            this.textBox8.TabIndex = 25;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(687, 378);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(186, 22);
+            this.textBox9.TabIndex = 26;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(589, 381);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 17);
+            this.label11.TabIndex = 27;
+            this.label11.Text = "Street*:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(595, 431);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(40, 17);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "City*:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(687, 422);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(186, 22);
+            this.textBox10.TabIndex = 29;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(595, 475);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(89, 17);
+            this.label13.TabIndex = 30;
+            this.label13.Text = "PostalCode*:";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(690, 468);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(186, 22);
+            this.textBox11.TabIndex = 31;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(690, 525);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(186, 22);
+            this.textBox12.TabIndex = 32;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(595, 525);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(88, 17);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "Car Number:";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(690, 574);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(186, 22);
+            this.textBox13.TabIndex = 34;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(541, 577);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(142, 17);
+            this.label15.TabIndex = 35;
+            this.label15.Text = "Card Expiration Date:";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(738, 625);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(86, 25);
+            this.button8.TabIndex = 36;
+            this.button8.Text = "Sign Up";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1460, 712);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +457,38 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBox13;
     }
 }
 
--- App/FoodDelivery/FoodDelivery/Form1.cs
+++ App/FoodDelivery/FoodDelivery/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,15 +37,10 @@ namespace FoodDelivery
             if (cn.State != ConnectionState.Open)
             {
                 cn.Open();
-                MessageBox.Show("Successful connection to database " + cn.Database + " on the " + cn.DataSource + " server", "Connection Test", MessageBoxButtons.OK);
+                //MessageBox.Show("Successful connection to database " + cn.Database + " on the " + cn.DataSource + " server", "Connection Test", MessageBoxButtons.OK);
             }
 
             return cn.State == ConnectionState.Open;
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -67,7 +63,99 @@ namespace FoodDelivery
             String logname = textBox1.Text;
             String logPass = textBox2.Text;
 
+            if (logname == string.Empty)
+            {
+                MessageBox.Show("Insert Username");
+            }
+            else if (logPass == string.Empty)
+            {
+                MessageBox.Show("Insert Password");
+            }
+            else {
+                SqlCommand cmd = new SqlCommand("FoodDelivery_FinalProject.uspLogin", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@pLoginName", SqlDbType.NVarChar, 50).Value = logname;
+                cmd.Parameters.Add("@pPassword", SqlDbType.NVarChar).Value = logPass;
+                cmd.Parameters.Add("@responseMessage", SqlDbType.NVarChar, 250).Direction = ParameterDirection.Output;
 
+                if (!verifySGBDConnection())
+                    return;
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("ola " + cmd.Parameters["@responseMessage"].Value);
+            }
+
+
+            
+            
+
+
+
+
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            //panel2.Visible = false;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                String filename = openFile.FileName;
+                textBox8.Text = filename;
+            }
+
+            /*string picturePath=//buscar
+
+            //Sign up
+            if (!(string.IsNullOrEmpty(picturePath))) {
+                using (Image image = Image.FromFile(picturePath)) {
+                    using (MemoryStream m = new MemoryStream()) {
+                        image.Save(m, image.RawFormat);
+                        byte[] imageBytes = m.ToArray();
+
+                        picturePath = Convert.ToBase64String(imageBytes);
+
+                    }
+
+                }
+            }*/
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
