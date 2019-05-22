31602538d5348d0fa622bdcf4be9b9fe33c23b9f
unknown <tomascosta@ua.pt>
2019-05-22 17:04:44 +0100

    fixed scripts and finish populating

--- Scripts/createTrip.py
+++ Scripts/createTrip.py
@@ -6,21 +6,28 @@ insert_trips = open('insert_trips.txt', 'w', encoding='utf8')
 
 trips_cost_file = open('trip_cost.txt', 'w', encoding='utf8')
 listDrivers = open('driver_id.txt', 'r', encoding='utf8')
+listRequests = open('request_id.txt', 'r', encoding='utf8')
 
 driver_IDS = []
+request_IDS = []
 
 for line in listDrivers:
   driver_IDS.append(line.strip('\n'))
 
+for line in listRequests:
+  request_IDS.append(line.strip('\n'))
+
 ids = 1
+ind = 0
 
 # 100 meals
-for i in range(100):
+for i in range(31):
     Trip_ID = ids
     ids+=1
 
     #Trip_ID
     driver_ID = random.choice(driver_IDS)
+    req_ID = request_IDS[ind]
     #cost
     trip_cost = round(random.uniform(5.0, 15.0),2)
 
@@ -31,9 +38,11 @@ for i in range(100):
     #for belongs table
     trips_cost_file.write("{}\n".format(trip_cost))
 
+    ind += 1
     #final
-    insert_trips.write("INSERT INTO FoodDelivery_FinalProject.Trip VALUES ('{}','{}','{}','{}');\n".format(driver_ID,trip_cost,time,distance))
+    insert_trips.write("INSERT INTO FoodDelivery_FinalProject.Trip VALUES ('{}','{}','{}','{}','{}');\n".format(driver_ID,trip_cost,time,distance,req_ID))
 
 insert_trips.close()
 listDrivers.close()
+listRequests.close()
 trips_cost_file.close()
